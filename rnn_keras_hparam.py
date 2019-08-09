e896c5cbba01bb552783c06e38f76b5ba4730c55
Ahmet Perilioglu <ahmetperilioglu@gmail.com>
2019-08-09 19:36:21 +0100

    weighted loss function

--- rnn_keras_hparam.py
+++ rnn_keras_hparam.py
@@ -11,7 +11,7 @@ from time import time
 
 n_inputs = 99 # total number of inputs minus 1 as it starts from 0
 n_outputs = 99
-n_windows = 50
+n_windows = 2
 start_train = 1000
 end_train = 11000
 end_test = 12000
@@ -63,14 +63,14 @@ from tensorflow.keras import optimizers
 from tensorflow.keras import initializers
 from tensorflow.keras import activations
 from tensorboard.plugins.hparams import api as hp
-import keras.backend as K
+from tensorflow.keras import backend as K
 from functools import partial
 
 HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([100]))
 HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.1,0.11))
 HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['rmsprop']))
-HP_OUTPUT = hp.HParam('output_number',hp.Discrete(list(range(1))))
-#HP_OUTPUT = hp.HParam('output_number',hp.Discrete(list(range(n_outputs-1))))
+#HP_OUTPUT = hp.HParam('output_number',hp.Discrete(list(range(1))))
+HP_OUTPUT = hp.HParam('output_number',hp.Discrete(list(range(n_outputs-1))))
 METRIC_ACCURACY = 'loss'
 
 hp.hparams_config(hparams=[HP_NUM_UNITS, HP_DROPOUT, HP_OPTIMIZER, HP_OUTPUT], metrics=[hp.Metric(METRIC_ACCURACY, display_name='loss')],)
@@ -82,8 +82,10 @@ def run(run_dir, hparams):
 
 optimizers.RMSprop(lr=0.001,rho=0.9,decay=0.9)
 
-#def weighted_mse(y_true, y_pred, weights):
-#    return K.mean(K.square(y_true - y_pred) * weights)
+def get_weighted_loss(weights):
+    def weighted_loss(y_true, y_pred):
+        return K.mean(K.square(y_true - y_pred)*weights,axis=-1)
+    return weighted_loss
 
 def train_test_model(run_dir,hparams):
     hp.hparams(hparams)
@@ -92,16 +94,16 @@ def train_test_model(run_dir,hparams):
     model.add(LSTM(hparams[HP_NUM_UNITS], return_sequences=True ,input_shape=(n_windows, n_inputs) ,activation='tanh' ,kernel_initializer='TruncatedNormal' ,bias_initializer=initializers.Constant(value=0.1), dropout=hparams[HP_DROPOUT] ,recurrent_dropout=hparams[HP_DROPOUT]))
     model.add(LSTM(hparams[HP_NUM_UNITS], activation='tanh' ,return_sequences=False ,kernel_initializer='TruncatedNormal' ,bias_initializer=initializers.Constant(value=0.1) ,dropout=hparams[HP_DROPOUT], recurrent_dropout=hparams[HP_DROPOUT]))
     model.add(Dense(units=99, activation='linear'))
-    weights = np.ones(y_train[:, 0:1, :].shape)
-    model.compile(optimizer=hparams[HP_OPTIMIZER], loss='mse') # metrics=['mae'])
+    weights = np.full([1,1,n_outputs],0.1)
+    weights[0,0,hparams[HP_OUTPUT]] = 1
+    model.compile(optimizer=hparams[HP_OPTIMIZER], loss=get_weighted_loss(weights=weights)) # metrics=['mae'])
     model.summary()
     #model.load_weights('model.h5')
-    model.fit(X_train, y_train[:, 0, :], epochs=100, batch_size=batch_size,
-              validation_data=(X_test, y_test[:, 0, :]))
+    #model.fit(X_train, y_train[:, 0, :], epochs=100, batch_size=batch_size, validation_data=(X_test, y_test[:, 0, :]))
     #model.fit(X_train, y_train[:,0,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1], epochs=1, batch_size=batch_size, validation_data=(X_test, y_test[:,0,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1]))
-    # model.fit(X_train, y_train[:,:,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1], epochs=1, batch_size=batch_size, validation_data=(X_test, y_test[:,:,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1]), callbacks=[
-    #      TensorBoard(log_dir=run_dir, histogram_freq=100, write_graph=True, write_grads=True, update_freq='epoch'),
-    #      hp.KerasCallback(writer=run_dir, hparams=hparams)])
+    model.fit(X_train, y_train[:,0,:], epochs=1000, batch_size=batch_size, validation_data=(X_test, y_test[:,0,:]), callbacks=[
+         TensorBoard(log_dir=run_dir, histogram_freq=100, write_graph=True, write_grads=True, update_freq='epoch'),
+         hp.KerasCallback(writer=run_dir, hparams=hparams)])
     #model.save_weights('model.h5')
     #sequence = n_windows
     #for iteration in range(30)
