d8b9e5d6baf24eeeb3008cc1c93ea3ab4676aafb
Ahmet Perilioglu <ahmetperilioglu@gmail.com>
2019-08-16 15:57:39 +0100

    rating data appended to the end

--- lstm_ratingmodel.py
+++ lstm_ratingmodel.py
@@ -24,8 +24,9 @@ from keras.utils import to_categorical
 
 # Part 1 - Data Preprocessing
 
-n_inputs = 58*14 # total number of inputs minus 1 as it starts from 0
-n_outputs = 58
+n_inputs = 14 # total number of inputs minus 1 as it starts from 0
+n_outputs = 1
+n_company = 58
 start_train = 0
 end_train = 96
 end_test = 126
@@ -34,22 +35,24 @@ batch_size = 12
 # Importing the training set
 data = pd.read_csv('rating_data.csv', index_col=0)
 
-Rating = data.values[:,0:n_outputs]
-Gross_Margin = data.values[:,1624:1624+n_outputs]
-Operating_Margin = data.values[:,2494:2494+n_outputs]
-Net_Profit_Margin = data.values[:,2320:2320+n_outputs]
-Return_on_Equity = data.values[:,2842:2842+n_outputs]
-Return_on_Assets = data.values[:,2784:2784+n_outputs]
-Current_Ratio = data.values[:,928:928+n_outputs]
-Liabilities_to_Equity_Ratio = data.values[:,1856:1856+n_outputs]
-Debt_to_Assets_Ratio = data.values[:,986:986+n_outputs]
-EV_EBITDA = data.values[:,1277:1277+n_outputs]
-EV_Sales = data.values[:,1334:1334+n_outputs]
-Book_to_Market = data.values[:,290:290+n_outputs]
-Operating_Income_EV = data.values[:,2436:2436+n_outputs]
-Share_Price = data.values[:,3074:3074+n_outputs]
+Rating = data.values[:,0:n_company]
+Gross_Margin = data.values[:,1624:1624+n_company]
+Operating_Margin = data.values[:,2494:2494+n_company]
+Net_Profit_Margin = data.values[:,2320:2320+n_company]
+Return_on_Equity = data.values[:,2842:2842+n_company]
+Return_on_Assets = data.values[:,2784:2784+n_company]
+Current_Ratio = data.values[:,928:928+n_company]
+Liabilities_to_Equity_Ratio = data.values[:,1856:1856+n_company]
+Debt_to_Assets_Ratio = data.values[:,986:986+n_company]
+EV_EBITDA = data.values[:,1277:1277+n_company]
+EV_Sales = data.values[:,1334:1334+n_company]
+Book_to_Market = data.values[:,290:290+n_company]
+Operating_Income_EV = data.values[:,2436:2436+n_company]
+Share_Price = data.values[:,3074:3074+n_company]
 
-data = np.concatenate((Rating,Gross_Margin, Operating_Margin, Net_Profit_Margin, Return_on_Equity, Return_on_Assets, Current_Ratio, Liabilities_to_Equity_Ratio, Debt_to_Assets_Ratio, EV_EBITDA, EV_Sales, Book_to_Market, Operating_Income_EV, Share_Price),axis=1)
+#data = np.concatenate((Rating,Gross_Margin, Operating_Margin, Net_Profit_Margin, Return_on_Equity, Return_on_Assets, Current_Ratio, Liabilities_to_Equity_Ratio, Debt_to_Assets_Ratio, EV_EBITDA, EV_Sales, Book_to_Market, Operating_Income_EV, Share_Price),axis=1)
+data = [Rating,Gross_Margin, Operating_Margin, Net_Profit_Margin, Return_on_Equity, Return_on_Assets, Current_Ratio, Liabilities_to_Equity_Ratio, Debt_to_Assets_Ratio, EV_EBITDA, EV_Sales, Book_to_Market, Operating_Income_EV, Share_Price]
+data = np.transpose(data)
 # Feature Scaling
 #sc = MinMaxScaler(feature_range = (0, 1))
 #data_scaled = sc.fit_transform(data)
@@ -62,16 +65,17 @@ y_sc = MinMaxScaler(feature_range=(0, 1))
 def create_data(data_scaled, start_train ,end_train ,n_windows):
     X_train = []
     y_train = []
-    for i in range(start_train, end_train):
-        X_train.append(data_scaled[i-n_windows:i, 0:n_inputs])
-        y_train.append(data_scaled[i:i+n_windows, 0:n_outputs])
+    for j in range(len(data_scaled)):
+        for i in range(start_train, end_train):
+            X_train.append(data_scaled[j][i-n_windows:i, 0:n_inputs])
+            y_train.append(data_scaled[j][i:i+n_windows, 0:n_outputs])
     X_train, y_train = np.array(X_train), np.array(y_train)
-    X_train = X_sc.fit_transform(np.reshape(X_train, (X_train.shape[0]* X_train.shape[1], n_inputs)))
-    y_train = y_sc.fit_transform(np.reshape(y_train, (y_train.shape[0]* y_train.shape[1], n_outputs)))
-    X_train = np.reshape(X_train, (end_train-start_train, n_windows, n_inputs))
-    y_train = np.reshape(y_train, (end_train-start_train, n_windows, n_outputs))
-    print(X_train.shape,y_train.shape)
-    return X_train, y_train
+    X_train_scaled = X_sc.fit_transform(np.reshape(X_train, (X_train.shape[0]* X_train.shape[1], X_train.shape[2])))
+    y_train_scaled = y_sc.fit_transform(np.reshape(y_train, (y_train.shape[0]* y_train.shape[1], y_train.shape[2])))
+    X_train_scaled = np.reshape(X_train_scaled, (X_train.shape[0], X_train.shape[1], X_train.shape[2]))
+    y_train_scaled = np.reshape(y_train_scaled, (y_train.shape[0], y_train.shape[1], y_train.shape[2]))
+    print(X_train_scaled.shape,y_train_scaled.shape)
+    return X_train_scaled, y_train_scaled
 
 # Part 2 - Building the RNN
 
@@ -102,26 +106,27 @@ def train_test_model(run_dir,hparams):
     hp.hparams(hparams)
     [X_train, y_train] = create_data(data_scaled=data, start_train=hparams[HP_WINDOW], end_train=end_train, n_windows=hparams[HP_WINDOW])
     [X_test, y_test] = create_data(data_scaled=data, start_train=end_train, end_train=end_test-hparams[HP_WINDOW], n_windows=hparams[HP_WINDOW])
-    pd.DataFrame(X_sc.inverse_transform(np.reshape(X_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:], ((end_test - end_train-hparams[HP_WINDOW]), n_inputs)))).to_csv('X.csv')
-    pd.DataFrame(y_sc.inverse_transform(np.reshape(y_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:], ((end_test - end_train-hparams[HP_WINDOW]), n_outputs)))).to_csv('y.csv')
+    pd.DataFrame(X_sc.inverse_transform(np.reshape(X_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:], (X_test.shape[0],X_test.shape[2])))).to_csv('X.csv')
+    pd.DataFrame(y_sc.inverse_transform(np.reshape(y_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:], (y_test.shape[0],y_test.shape[2])))).to_csv('y.csv')
     tf.compat.v1.keras.backend.clear_session()
     model = Sequential()
     model.add(LSTM(hparams[HP_NUM_UNITS], unit_forget_bias=True, return_sequences=True ,input_shape=(hparams[HP_WINDOW], n_inputs) ,activation='tanh' ,kernel_initializer='TruncatedNormal' ,bias_initializer=initializers.Constant(value=0.1), dropout=hparams[HP_DROPOUT] ,recurrent_dropout=hparams[HP_DROPOUT]))
     #model.add(LSTM(hparams[HP_NUM_UNITS], activation='tanh' ,return_sequences=False ,kernel_initializer='TruncatedNormal' ,bias_initializer=initializers.Constant(value=0.1) ,dropout=hparams[HP_DROPOUT], recurrent_dropout=hparams[HP_DROPOUT]))
     model.add(Dense(units=n_outputs, activation='linear'))
-    weights = np.full([1,1,n_outputs],1)
+    #weights = np.full([1,1,n_outputs],1)
     #weights[0,0,0:57] = 1
-    model.compile(optimizer=hparams[HP_OPTIMIZER], loss=get_weighted_loss(weights=weights)) # metrics=['mae'])
+    model.compile(optimizer=hparams[HP_OPTIMIZER], loss='mse') #get_weighted_loss(weights=weights)) # metrics=['mae'])
     model.summary()
     #model.load_weights('model.h5')
-    model.fit(X_train, y_train[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], :], epochs=20, validation_data=(X_test, y_test[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], :]))
+    model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+    #model.fit(X_train, y_train[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], :], epochs=10, validation_data=(X_test, y_test[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], :]))
     #model.fit(X_train, y_train[:,0,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1], epochs=1, batch_size=batch_size, validation_data=(X_test, y_test[:,0,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1]))
     #model.fit(X_train, y_train[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:], epochs=10, batch_size=batch_size, validation_data=(X_test, y_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:]), callbacks=[
     #    TensorBoard(log_dir=run_dir, histogram_freq=50, write_graph=True, write_grads=True, update_freq='epoch'),
     #    hp.KerasCallback(writer=run_dir, hparams=hparams)])
     #model.save_weights('model.h5')
-    X_test_extended = X_test
-    predicted = model.predict(X_test_extended)
+    #X_test_extended = X_test
+    predicted = model.predict(X_test)
     # for iteration in range(hparams[HP_WINDOW]):
     #     predicted = model.predict(X_test_extended)
     #     if hparams[HP_WINDOW] != 1:
