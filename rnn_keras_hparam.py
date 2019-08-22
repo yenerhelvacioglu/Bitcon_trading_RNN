97ecc7e1f1db86e5371516dc080e38af85bd3b56
Ahmet <yenerhelvacioglu@gmail.com>
2019-08-22 21:58:54 +0100

    some fixes

--- rnn_keras_hparam.py
+++ rnn_keras_hparam.py
@@ -95,7 +95,7 @@ def train_test_model(run_dir,hparams):
     [X_train, y_train] = create_data(data_scaled=data_scaled, start_train=start_train, end_train=end_train, n_windows=hparams[HP_WINDOW])
     [X_test, y_test] = create_data(data_scaled=data_scaled, start_train=end_train, end_train=end_test, n_windows=hparams[HP_WINDOW])
     #pd.DataFrame(np.reshape(X_test, (end_test - end_train, n_inputs))).to_csv('X.csv')
-    #pd.DataFrame(np.reshape(y_test, (end_test - end_train, n_outputs))).to_csv('y.csv')
+    pd.DataFrame(y_test[:,:,0]).to_csv('y.csv')
     tf.compat.v1.keras.backend.clear_session()
     model = Sequential()
     model.add(LSTM(hparams[HP_NUM_UNITS], return_sequences=False ,input_shape=(hparams[HP_WINDOW], n_inputs) ,activation='tanh' ,kernel_initializer='TruncatedNormal' ,bias_initializer=initializers.Constant(value=0.1), dropout=hparams[HP_DROPOUT] ,recurrent_dropout=hparams[HP_DROPOUT]))
@@ -106,7 +106,7 @@ def train_test_model(run_dir,hparams):
     model.compile(optimizer=hparams[HP_OPTIMIZER], loss=get_weighted_loss(weights=weights)) # metrics=['mae'])
     model.summary()
     #model.load_weights('model.h5')
-    model.fit(X_train, y_train[:, 0, :], epochs=10, batch_size=batch_size, validation_data=(X_test, y_test[:, 0, :]))
+    model.fit(X_train, y_train[:, 0, :], epochs=1, batch_size=batch_size, validation_data=(X_test, y_test[:, 0, :]))
     #model.fit(X_train, y_train[:,0,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1], epochs=1, batch_size=batch_size, validation_data=(X_test, y_test[:,0,hparams[HP_OUTPUT]:hparams[HP_OUTPUT]+1]))
     # model.fit(X_train, y_train[:,0,:], epochs=1, batch_size=batch_size, validation_data=(X_test, y_test[:,0,:]), callbacks=[
     #      TensorBoard(log_dir=run_dir, histogram_freq=10, write_graph=True, write_grads=True, update_freq='epoch'),
@@ -140,4 +140,4 @@ for num_units in HP_NUM_UNITS.domain.values:
                     run_dir = os.path.join("logs", run_name)
                     predicted = run(run_dir, hparams)
                     #pd.DataFrame(np.reshape(predicted, ((end_test-end_train),n_outputs))).to_csv('pred' +run_name+'.csv')
-                    pd.DataFrame(np.reshape(sc.inverse_transform(predicted[:,-1]), ((end_test-end_train),n_outputs))).to_csv('pred' +run_name+'.csv')
+                    pd.DataFrame(predicted[:,:,0]).to_csv('pred' +run_name+'.csv')
