c305b790acfba43a011b86e1d0c3591cd909a6ca
neuralnetworktrading <neuralnetworktrading@gmail.com>
2019-09-15 13:54:47 +0000

    save weights

--- rnn_keras_hparam.py
+++ rnn_keras_hparam.py
@@ -31,7 +31,7 @@ batch_size = 1000
 
 # Importing the training set
 data = pd.read_csv('5minsData.csv', index_col=0)
-data = data.values[:,::2]
+data = data.values[:,:]
 
 # Feature Scaling
 X_sc = MinMaxScaler(feature_range=(0, 1))
@@ -56,11 +56,11 @@ def create_data(data_unscaled, start_train ,end_train ,n_windows,n_outputs):
 # Part 2 - Building the RNN
 
 HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([100]))
-HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.1,0.11))
+HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.2,0.21))
 HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['rmsprop']))
-HP_OUTPUT = hp.HParam('output_number',hp.Discrete(list(range(0,3))))
+HP_OUTPUT = hp.HParam('output_number',hp.Discrete(list(range(0,40))))
 HP_WINDOW = hp.HParam('window_size',hp.Discrete([50]))
-HP_HORIZON = hp.HParam('horizon_size',hp.Discrete([25]))
+HP_HORIZON = hp.HParam('horizon_size',hp.Discrete([50]))
 #HP_OUTPUT = hp.HParam('output_number',hp.Discrete(list(range(n_outputs-1))))
 METRIC_ACCURACY = 'loss'
 
@@ -83,11 +83,12 @@ def train_model(run_dir,hparams):
     model.add(Dense(units=1, activation='linear'))
     model.compile(optimizer=hparams[HP_OPTIMIZER], loss='mse') #get_weighted_loss(weights=weights)) # metrics=['mae'])
     model.summary()
-    model.fit(X_train, y_train[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], 0], epochs=1, validation_data=(X_test, y_test[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], 0]))
-    #model.fit(X_train, y_train[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],0], epochs=1, batch_size=batch_size, validation_data=(X_test, y_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],0]), callbacks=[
-    #    TensorBoard(log_dir=run_dir, histogram_freq=10, write_graph=True, write_grads=True, update_freq='epoch'),
-    #    hp.KerasCallback(writer=run_dir, hparams=hparams)])
+    #model.fit(X_train, y_train[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], 0], epochs=1, validation_data=(X_test, y_test[:, hparams[HP_WINDOW]-1:hparams[HP_WINDOW], 0]))
+    model.fit(X_train, y_train[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],0], epochs=100, batch_size=batch_size, validation_data=(X_test, y_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],0]), callbacks=[
+        TensorBoard(log_dir=run_dir, histogram_freq=100, write_graph=True, write_grads=True, update_freq='epoch'),
+        hp.KerasCallback(writer=run_dir, hparams=hparams)])
     model.save('model_' + str(hparams[HP_NUM_UNITS]) + '_' + str(hparams[HP_DROPOUT]) + '_' + str(hparams[HP_OPTIMIZER]) + '_' + str(hparams[HP_WINDOW]) + '_' + str(hparams[HP_OUTPUT]) + '.h5')
+    model.save_weights('model_weights_' + str(hparams[HP_NUM_UNITS]) + '_' + str(hparams[HP_DROPOUT]) + '_' + str(hparams[HP_OPTIMIZER]) + '_' + str(hparams[HP_WINDOW]) + '_' + str(hparams[HP_OUTPUT]) + '.h5')
 
     return 0
 
@@ -96,14 +97,16 @@ def test_model(hparams):
     [X_test, y_test] = create_data(data_unscaled=data, start_train=end_train, end_train=end_test, n_windows=hparams[HP_WINDOW], n_outputs=0)
     pd.DataFrame(X_sc.inverse_transform(np.reshape(X_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:], (X_test.shape[0],X_test.shape[2])))).to_csv('X_'+ str(hparams[HP_WINDOW]) +'.csv')
     pd.DataFrame(y_sc.inverse_transform(np.reshape(y_test[:,hparams[HP_WINDOW]-1:hparams[HP_WINDOW],:], (y_test.shape[0],y_test.shape[2])))).to_csv('y_'+ str(hparams[HP_WINDOW]) +'.csv')
+    
+    tf.compat.v1.keras.backend.clear_session()
+    model = load_model('model_' + str(hparams[HP_NUM_UNITS]) + '_' + str(hparams[HP_DROPOUT]) + '_' + str(hparams[HP_OPTIMIZER]) + '_' + str(hparams[HP_WINDOW]) + '_' + '0' + '.h5')
 
     X_test_extended = X_test
     for iteration_w in range(hparams[HP_WINDOW]):
         predicted = []
         for iteration_o in range(hparams[HP_OUTPUT]):
-            tf.compat.v1.keras.backend.clear_session()
-            model = load_model('model_' + str(hparams[HP_NUM_UNITS]) + '_' + str(hparams[HP_DROPOUT]) + '_' + str(
-                hparams[HP_OPTIMIZER]) + '_' + str(hparams[HP_WINDOW]) + '_' + str(iteration_o) + '.h5')
+            #tf.compat.v1.keras.backend.clear_session()
+            model.load_weights('model_weights_' + str(hparams[HP_NUM_UNITS]) + '_' + str(hparams[HP_DROPOUT]) + '_' + str(hparams[HP_OPTIMIZER]) + '_' + str(hparams[HP_WINDOW]) + '_' + str(iteration_o) + '.h5')
             print(str(iteration_w) + ' ' + str(iteration_o))
             predicted.append(model.predict(X_test_extended))
         predicted = np.transpose(predicted,axes=(1,2,0))
@@ -136,4 +139,4 @@ for num_units in HP_NUM_UNITS.domain.values:
                     run_dir = os.path.join("logs/hparam_tuning/", run_name)
                     #train_model(run_dir, hparams)
 
-test_model({HP_NUM_UNITS: 100, HP_DROPOUT: round(0.1,1), HP_OPTIMIZER: 'rmsprop', HP_OUTPUT: 3, HP_WINDOW: 50, HP_HORIZON:[1,5,10,25,50]})
+test_model({HP_NUM_UNITS: 100, HP_DROPOUT: round(0.2,1), HP_OPTIMIZER: 'rmsprop', HP_OUTPUT: 40, HP_WINDOW: 50, HP_HORIZON:[1,5,10,25,50]})
